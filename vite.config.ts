a6b26a65e0d512c736ded66719496a19b4eea811
Harish <dev.harish3124@gmail.com>
2023-10-29 00:29:20 +0530

    Add assets

--- vite.config.ts
+++ vite.config.ts
@@ -1,40 +1,18 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import Icons from "./public/icons.json";
 
 import { VitePWA, VitePWAOptions } from "vite-plugin-pwa";
 
 const manifestForPlugin: Partial<VitePWAOptions> = {
   registerType: "prompt",
-  includeAssets: ["favicon.ico", "apple-touch-icon.png", "masked-icon.svg"],
+  // includeAssets: ["favicon.ico", "apple-touch-icon.png", "masked-icon.svg"],
   manifest: {
     name: "ChatUI",
     short_name: "ChatUI",
     description:
       "An app that retrives data from a API and displays it as a chat",
-    icons: [
-      {
-        src: "/android-chrome-192x192.png",
-        sizes: "192x192",
-        type: "image/png",
-      },
-      {
-        src: "/android-chrome-512x512.png",
-        sizes: "512x512",
-        type: "image/png",
-      },
-      {
-        src: "/apple-touch-icon.png",
-        sizes: "180x180",
-        type: "image/png",
-        purpose: "apple touch icon",
-      },
-      {
-        src: "/maskable_icon.png",
-        sizes: "225x225",
-        type: "image/png",
-        purpose: "any maskable",
-      },
-    ],
+    icons: Icons.icons,
     theme_color: "#FAF9F4",
     background_color: "#FAF9F4",
     display: "standalone",
